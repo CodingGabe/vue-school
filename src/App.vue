31ae917a949ff7323d15c9950c417b18ea7b6dcc
Gabe Amaya <gabeamaya2@gmail.com>
2022-12-09 14:15:19 -0600

    complete shopping list app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: 'HellowWorld',
   components: {
     HelloWorld
   }
@@ -15,12 +14,18 @@ export default {
 </script>
 
 <style>
+html {
+  background-color: rgb(226 232 240);
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  max-width: 54rem;
+  margin: 3.75rem auto;
+  padding: 2.5rem 4rem;
+  background-color: #fff;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,147 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <!-- Heading -->
+  <div class="header">
+    <h1>{{ msg || 'My Shopping List' }}</h1>
+    <!-- <button class="btn-secondary" @click="toggle">Add Item</button> -->
   </div>
+  <!-- Add itemm, and prioritize -->
+  <form @submit.prevent="saveItem" class="add-item-form">
+    <!-- Binds user input -->
+    <input v-model.trim="newItem" type="text" placeholder="Add item">
+    <label for="priority" value="High Priority">
+      High Priority
+      <input type="checkbox" v-model="newItemHighPriority">
+    </label>
+    <!-- save button -->
+    <button class="btn btn-primary">Save Item</button>
+  </form>
+  <!-- Start of the shopping list -->
+  <ul>
+    <li @click="toggleDone(item)" v-for="item in reversedItems" :key="item.id">
+      <span class="list-item" :class="{ done: item.done, priority: item.highPriority }">{{ item.label }}</span>
+      <button class="close" @click="removeItem(item)">Remove</button>
+    </li>
+  </ul>
+  <p v-if="items.length === 0">Nothing on the list 😞</p>
 </template>
 
 <script>
+// set id to a value to start
+let id = 0
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      newItem: '', // create empty array to start the item
+      newItemHighPriority: false, // sets the priority to off initially
+      editing: false,
+      items: [
+        { id: id++, label: 'carrots', done: true, highPriority: false },
+        { id: id++, label: 'apples', done: true, highPriority: false },
+        { id: id++, label: 'oranges', done: false, highPriority: true }
+      ]
+    }
+  },
+  computed: {
+    reversedItems () {
+      return [...this.items].reverse()
+    }
+  },
+  methods: {
+    saveItem () {
+      this.items.push({ id: id++, label: this.newItem, done: false, highPriority: this.newItemHighPriority }) // adding a new item with a new id by pushing on to the array
+      this.newItem = '' // clears out the form
+      this.newItemHighPriority = ''
+    },
+    removeItem (item) {
+      this.items = this.items.filter((t) => t !== item)
+    },
+    toggleDone (item) {
+      item.done = !item.done
+    },
+    toggle () {
+      this.editing = !this.editing
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.priority {
+  color: rgb(22 163 74);
+  font-weight: 700;
+}
+.done {
+  cursor: pointer;
+  text-decoration: line-through;
+}
+form {
+  margin-bottom: 2.5rem;
+}
+button.close {
+  display: inline-block;
+  vertical-align: middle;
+  background-color: rgb(239 68 68);
+  color: #fff;
+  border: none;
+  border-radius: 3px;
+  padding: .35rem .45rem;
+  margin-left: 1rem;
+}
+.btn-secondary {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing:antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 1rem;
+  background-color: rgb(29 78 216);
+  border: none;
+  border-radius: 3px; color: #fff;
+  font-weight: 700;
+  display: inline-block;
+  margin-bottom: 1.875rem;
+  padding: .75rem 1rem;
+}
+.add-item-form .btn {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing:antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 1rem;
+  background-color: rgb(79 70 229);
+  border: none;
+  border-radius: 3px;
+  color: #fff;
+  font-weight: 700;
+  display: inline-block;
+  margin-left: 1rem;
+  padding: .75rem 1rem;
+}
+.add-item-form label {
+  padding-left: 1rem;
+}
+.add-item-form input[type=text] {
+  border: none;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  padding: .75rem;
+  font-size: 1rem;
+}
+.add-item-form input[type=text]::placeholder {
+  font-size: 1rem;
+  color: rgb(148 163 184);
+}
+.add-item-form input[type=checkbox] {
+  vertical-align: middle;
+}
+h1 {
+  font-weight: 700;
+  font-size: 3rem;
+  letter-spacing: -.75px;
+  line-height: 1.2;
+}
 h3 {
   margin: 40px 0 0;
 }
@@ -49,10 +150,30 @@ ul {
   padding: 0;
 }
 li {
-  display: inline-block;
+  cursor: pointer;
+  display: block;
   margin: 0 10px;
+}
+li span {
+  display: inline-block;
+  padding: .75rem;
+}
+li span:hover {
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
 }
 a {
   color: #42b983;
+}
+ul {
+  display: flex;
+  flex-direction: column;
+}
+li {
+  font-size: 1.5rem;
+  margin-bottom: .625rem;
+}
+p {
+  font-size: 1.5rem;
+  font-weight: 700;
 }
 </style>
